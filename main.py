e0ce6ee83d15a3fc59d4bf1a4f1122c14756f6c9
crissynealon <915620338@qq.com>
2024-11-24 00:31:00 +0800

    add hqc128 testing code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from binascii import hexlify
 # from pqcrypt.kem.tm import generate_keypair, encrypt, decrypt
-from pqcrypt.kem.firesaber import keygen, encaps, decaps
+# from pqcrypt.kem.firesaber import keygen, encaps, decaps
+from pqcrypt.kem.hqc128 import keygen, encaps, decaps, encrypt, decrypt
 
 def firesaber():
     pk, sk = keygen()
@@ -14,15 +15,28 @@ def firesaber():
     print("ss0", hexlify(ss0))
 
 def hqc128():
+    print('hqc128')
+    # import ipdb; ipdb.set_trace();
     pk, sk = keygen()
-    print("pk", hexlify(pk))
-    print("sk", hexlify(sk))
+    # print("pk", hexlify(pk))
+    # print("sk", hexlify(sk))
 
     ct, ss = encaps(pk)
-    print("ss", hexlify(ss))
+    # print("ss", hexlify(ss))
 
     ss0 = decaps(sk, ct)
-    print("ss0", hexlify(ss0))
+    # print("ss0", hexlify(ss0))
+
+    ciphertext = encrypt(b'a'*0x10, pk)
+    print("cipher:")
+    print(hexlify(ciphertext))
+
+    plaintext = decrypt(ciphertext, sk)
+    print("plain:")
+    print(hexlify(plaintext))
+
+    assert(plaintext == b'a'*0x10)
+
 
 def main():
     pk, sk = generate_keypair()
