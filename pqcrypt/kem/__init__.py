937430f31bb6e96bd0664c84b975ad6a37a4d09b
crissynealon <915620338@qq.com>
2024-11-26 16:55:13 +0800

    add interfaces

---
+++ pqcrypt/kem/__init__.py
@@ -0,0 +1,37 @@
+from . import firesaber as firesaber
+from . import lightsaber as lightsaber
+from . import saber as saber
+from . import hqc128 as hqc128
+from . import hqc192 as hqc192
+from . import hqc256 as hqc256
+from . import mlkem512 as mlkem512
+from . import mlkem768 as mlkem768
+from . import mlkem1024 as mlkem1024
+from . import kyber512 as kyber512
+from . import kyber512_90s as kyber512_90s
+from . import kyber768 as kyber768
+from . import kyber768_90s as kyber768_90s
+from . import kyber1024 as kyber1024
+from . import kyber1024_90s as kyber1024_90s
+from . import ntruhps2048509 as ntruhps2048509
+from . import ntruhps2048677 as ntruhps2048677
+from . import ntruhps4096821 as ntruhps4096821
+from . import ntruhrss701 as ntruhrss701
+from . import mceliece348864_clean as mceliece348864
+from . import mceliece348864f_clean as mceliece348864f
+from . import mceliece460896_clean as mceliece460896
+from . import mceliece460896f_clean as mceliece460896f
+from . import mceliece6688128_clean as mceliece6688128
+from . import mceliece6688128f_clean as mceliece6688128f
+from . import mceliece6960119_clean as mceliece6960119
+from . import mceliece6960119f_clean as mceliece6960119f
+from . import mceliece8192128_clean as mceliece8192128
+from . import mceliece8192128f_clean as mceliece8192128f
+# from . import fordokem640aes as fordokem640aes
+# from . import fordokem640shake as fordokem640shake
+# from . import fordokem976aes as fordokem976aes
+# from . import fordokem976shake as fordokem976shake
+# from . import fordokem1344aes as fordokem1344aes
+# from . import fordokem1344shake as fordokem1344shake
+# from . import sntryp761 as sntryp761
+# from . import bike as bike
